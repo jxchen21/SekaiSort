aeaf5314ada6b21ed726f6f8bec1dd197123de7b
jxchen21 <120130505+jxchen21@users.noreply.github.com>
2025-06-13 22:10:46 -0400

    Update favicon and metadata

--- client/src/app/components/Dropzone.tsx
+++ client/src/app/components/Dropzone.tsx
@@ -11,7 +11,7 @@ interface props {
 
 
 export default function Dropzone(props: props) {
-  const API_BASE_URL = "https://sekai-sort-server.up.railway.app";
+  const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
   interface Rank {
     filename: string;
     tier: number;
