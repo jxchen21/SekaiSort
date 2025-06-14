b5cf30560c6133b7f16b09f891f340d72c6f5687
jxchen21 <120130505+jxchen21@users.noreply.github.com>
2025-06-14 01:15:17 -0400

    Fix minor prod error

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -4,10 +4,10 @@ export default function Home() {
         <Image src="/images/welcome.png" alt="Welcome" width={500} height={50} className="mb-5"/>
         <Image src="/images/banner.png" alt="Banner" width={3000} height={100} />
         <p className="bg-white w-[75%] rounded-2xl px-10 py-5 mt-5 text-mid-blue font-semibold text-xl">
-            Hey! Welcome to sekai.sort. I made this tool to help Project Sekai players show off
+            {`Hey! Welcome to sekai.sort. I made this tool to help Project Sekai players show off
             their tiers without all the hassle of cropping, removing backgrounds, and formatting
             screenshots. Why? Definitely not because I'm unemployed haha... Anyways, hopefully you find this
-            useful, and check out the Usage Guide for info on how to get started! Enjoy :D - Arctic
+            useful, and check out the Usage Guide for info on how to get started! Enjoy :D - Arctic`}
         </p>
     </>);
 }
