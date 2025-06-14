ffd78022e55398c289e59cab8405cc9c298cd14b
jxchen21 <120130505+jxchen21@users.noreply.github.com>
2025-06-14 12:32:10 -0400

    Fix about image

--- client/src/app/about/page.tsx
+++ client/src/app/about/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 export default function About() {
     return (<>
-    <Image src={'/images/about.png'} alt="Guide" width={150} height={50} className="mb-5"/>
+    <Image src={'/images/about.png'} alt="about" width={150} height={50} className="mb-5"/>
     <p className="bg-white w-[75%] rounded-2xl px-10 py-5 mt-5 text-mid-blue font-semibold text-xl text-center mb-5">
         {`Made with 💖 by Arctic for RIIA (And the rest of the PJSK Community!)`}
         <br />
